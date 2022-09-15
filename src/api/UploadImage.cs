7a3ebe0658ad13d95c5963d55f0eb51932fc4090
Aaron Powell <me@aaron-powell.com>
2022-09-15 07:02:18 +0000

    Scaffold for the upload file endpoint

---
+++ src/api/UploadImage.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Contoso;
+
+public static class UploadImage
+{
+    [FunctionName("UploadImage")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
+    {
+        return new OkResult();
+    }
+}

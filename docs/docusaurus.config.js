9f556cd95a6c7f619e77c0c43fc46817e18edb2d
Dave Glover <dglover@microsoft.com>
2024-04-23 11:39:58 +1000

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "New Patient Registration Demo",
   tagline: "New Patient Registration Azure AI Document Intelligence Workshop",
 
-  url: "https://patiente2e.github.io/",
+  url: "https://Azure-Samples.github.io/",
   baseUrl: "/azure-doc-intelligence-dynamic-models-patient/",
 
   onBrokenLinks: "throw",
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "newpatiente2e", // Usually your GitHub org/user name.
+  organizationName: "Azure-Samples", // Usually your GitHub org/user name.
   projectName: "azure-doc-intelligence-dynamic-models-patient", // Usually your repo name.
 
   deploymentBranch: "gh-pages", // Branch that GitHub pages will deploy from.
